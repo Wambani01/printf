fcc9affa730c4513552fc43702fe31eaa9efab69
njogued <njogued@gmail.com>
2022-10-19 06:57:42 -0700

    Printcs

--- printcs2.c
+++ printcs2.c
@@ -4,67 +4,68 @@
  * @format: string of items
  * return : 0 on success
  */
-int _printf(const char* format, ...)
+int _printf(const char *format, ...)
 {
-    va_list list;
-    char chr;
-    char *str;
-    int i = 0, count = 0, strl = 0, ccount = 0, intgr = 0;
-    va_start(list, format);
+	va_list list;
+	char chr;
+	char *str;
+	int i = 0, count = 0, strl = 0, ccount = 0, intgr = 0;
 
-    for(i = 0; format[i] != '\0'; i++)
-    {
-        if(format[i] == 37)
-        {
-            i++;
-            if(format[i] == 'c')
-            {
-                chr = va_arg(list, int);
-                if(!chr)
-                {
-                    ccount = prnt_string("(null)");
-		    count = count + ccount;
-                }
-                else
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == 37)
 		{
-			_putchar(chr);
-			count++;
+			i++;
+			if (format[i] == 'c')
+			{
+				chr = va_arg(list, int);
+				if (!chr)
+				{
+					ccount = prnt_string("(null)");
+					count = count + ccount;
+				}
+				else
+				{
+					_putchar(chr);
+					count++;
+				}
+			}
+			else if (format[i] == 'd')
+			{
+				intgr = va_arg(list, int);
+				if (intgr < 10)
+				{
+					_putchar(intgr + '0');
+				}
+			}
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(list, char*);
+				if (!str)
+				{
+					ccount = prnt_string("(null)");
+					count = count + ccount;
+				}
+				else
+				{
+					strl = prnt_string(str);
+					count = count + strl;
+				}
+			}
 		}
-            }
-            else if (format[i] == 'd')
-            {
-                intgr = va_arg(list, int);
-                if(intgr < 10)
-                {
-                    _putchar(intgr + '0');
-                }
-            }
-            else if(format[i] == '%')
-            {
-                _putchar('%');
-		count++;
-            }
-            else if (format[i] == 's')
-            {
-                str = va_arg(list, char*);
-                if(!str)
-                {
-                    ccount = prnt_string("(null)");
-		    count = count + ccount;
-                }
 		else
 		{
-			strl = prnt_string(str);
-			count = count + strl;
+			_putchar(format[i]);
+			count++;
 		}
-            }
-        }
-        else
-	{
-		_putchar(format[i]);
-		count++;
 	}
-    }
-    va_end(list);
-    return (count);
+	va_end(list);
+	return (count);
 }
