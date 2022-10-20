716742390b660690fc5c5fc36faef538d1bea2ae
njogued <njogued@gmail.com>
2022-10-20 12:47:32 -0700

    In progress

---
+++ inprogress/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+	char* test;
+
+	printf("char%sn\n", test);
+	return (0);
+}
---
+++ inprogress/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+int prnt_string(char *string);
+int charfunc(char chr);
+int strfunc(char *str);
+#endif
