dc03f88a4491afc29e8da42b4f08948bc2e621a7
Wambani01 <georgewmbn@gmail.com>
2022-10-20 06:22:25 -0700

    task 0

--- printcs2.c
+++ printcs2.c
@@ -7,50 +7,54 @@
  */
 int _printf(const char *format, ...)
 {
+	if (format == NULL)
+	{
+		return (-1);
+	}
 	va_list list;
+	
+	if (list == NULL)
+	{
+		return (-1);
+	}
 	char chr;
 	char *str;
 	int i = 0, count = 0;
 
 	va_start(list, format);
 
-	if (format == NULL || list == NULL)
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		return (-1);
-	}
-	else
-	{
-		for (i = 0; format[i] != '\0'; i++)
+		if (format[i] == 37)
 		{
-			if (format[i] == 37)
+			i++;
+			if (format[i] == 'c')
 			{
-				i++;
-				if (format[i] == 'c')
-				{
-					chr = va_arg(list, int);
-					count += charfunc(chr);
-				}
-				else if (format[i] == '%')
-				{
-					count += prnt_string("%");
-				}
-				else if (format[i] == 's')
-				{
-					str = va_arg(list, char*);
-					count += strfunc(str);
-				}
-				else
-				{
-					count += charfunc('%');
-					count += charfunc(format[i]);
-				}
+				chr = va_arg(list, int);
+				count += charfunc(chr);
+			}
+			else if (format[i] == '%')
+			{
+				count += prnt_string("%");
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(list, char*);
+				count += strfunc(str);
 			}
 			else
 			{
+				count += charfunc('%');
 				count += charfunc(format[i]);
 			}
 		}
+		else
+		{
+			count += charfunc(format[i]);
+		}
 	}
+
 	va_end(list);
 	return (count);
 } 
